92d5072673866da68206961fbf7ba8c48a5f9a2a
Fabian Alleblas <fabian@alleblas.de>
2020-11-23 19:00:01 +0100

    Fix typos in globalstyles

--- frontend/src/globalstyles/GlobalStyle.js
+++ frontend/src/globalstyles/GlobalStyle.js
@@ -4,10 +4,10 @@ export default createGlobalStyle`
 :root {
     --primary-light: rgb(252,252,252);
     --primary-dark: rgb(110,217,171);
-    --form-font-color: rgb(46,59,56)
-    --input-font-color: rgb(183,189,187)
-    --primary-plant-font-color: rgb(95,122,117)
-    --secondary-plant-font-color: rgb(145,186,179)
+    --form-font-color: rgb(46,59,56);
+    --input-font-color: rgb(183,189,187);
+    --primary-plant-font-color: rgb(95,122,117);
+    --secondary-plant-font-color: rgb(145,186,179);
     --warning-color: rgb(229,27,27);
     --bar-bg-color: rgb(229,229,229);
     --bar-water-color: rgb(27,108,229);

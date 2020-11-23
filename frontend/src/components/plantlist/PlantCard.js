341460d5f0e2767dfbf78a26fbe810a07fe8bbd9
Fabian Alleblas <fabian@alleblas.de>
2020-11-23 17:00:39 +0100

    Add PlantCard component and PlantListPage

---
+++ frontend/src/components/plantlist/PlantCard.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components/macro'
+
+export default function PlantCard() {
+  return (
+    <div>
+      <div></div>
+      <p></p>
+      <p></p>
+    </div>
+  )
+}
---
+++ frontend/src/components/plantlist/PlantListPage.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/macro'
+import PlantCard from './PlantCard'
+
+export default function PlantListPage() {
+  return (
+    <ItemContainer>
+      <PlantCard />
+    </ItemContainer>
+  )
+}
+
+const ItemContainer = styled.section``

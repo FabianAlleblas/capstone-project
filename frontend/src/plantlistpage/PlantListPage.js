52c59bf7b837e09e6a907315b9ecf92424ec2758
Fabian Alleblas <fabian@alleblas.de>
2020-11-23 17:04:24 +0100

    Update folder structure

--- frontend/src/plantlistpage/PlantListPage.js
+++ frontend/src/plantlistpage/PlantListPage.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components/macro'
-import PlantCard from './PlantCard'
+import PlantCard from '../components/plantcard/PlantCard'
 
 export default function PlantListPage() {
   return (

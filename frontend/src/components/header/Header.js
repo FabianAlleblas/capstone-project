a503ea22331275ab4d121b6a4fac22e4ac935418
Fabian Alleblas <fabian@alleblas.de>
2020-11-23 19:49:32 +0100

    Add header

---
+++ frontend/src/components/header/Header.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components/macro'
+
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <h1>My Plants</h1>
+      </div>
+    </header>
+  )
+}

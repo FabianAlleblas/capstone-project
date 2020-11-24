704557704c3701a160893a6a0ade2776ce652f50
Fabian Alleblas <fabian@alleblas.de>
2020-11-24 16:48:26 +0100

    Add AddButton to PlantListPage

--- frontend/src/plantlistpage/PlantListPage.js
+++ frontend/src/plantlistpage/PlantListPage.js
@@ -1,24 +1,28 @@
 import styled from 'styled-components/macro'
 import Header from '../components/header/Header'
 import PlantCard from '../components/plantcard/PlantCard'
+import AddButton from '../components/buttons/AddButton'
 import plants from '../data/plants.json'
 
 export default function PlantListPage() {
   return (
     <>
-      <PositionWrapper>
+      <PositionHeader>
         <Header />
-      </PositionWrapper>
+      </PositionHeader>
       <ItemContainer>
         {plants.map(({ id, name, species }) => (
           <PlantCard key={id} name={name} species={species} />
         ))}
       </ItemContainer>
+      <PositionButton>
+        <AddButton />
+      </PositionButton>
     </>
   )
 }
 
-const PositionWrapper = styled.div`
+const PositionHeader = styled.div`
   position: fixed;
   top: 0;
   left: 0;
@@ -29,4 +33,9 @@ const ItemContainer = styled.section`
   gap: 40px 20px;
   grid-template-columns: 1fr 1fr;
   padding: 100px 20px 20px;
+`
+const PositionButton = styled.div`
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
 `

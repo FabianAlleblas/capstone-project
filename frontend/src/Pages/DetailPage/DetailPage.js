3b5fa489ad517edd6ec527204cab4edf9b3347aa
Fabian Alleblas <fabian@alleblas.de>
2020-11-28 14:14:16 +0100

    Add arrowicon to header

--- frontend/src/Pages/DetailPage/DetailPage.js
+++ frontend/src/Pages/DetailPage/DetailPage.js
@@ -1,15 +1,18 @@
 import { useLocation } from 'react-router-dom'
 import Header from '../../Components/Header/Header'
-import PlantCard from '../../Components/Plantcard/PlantCard'
+import { useHistory } from 'react-router-dom'
 
 export default function DetailPage({ plantList }) {
+  const history = useHistory()
   const query = useQuery()
   const plantId = parseInt(query.get('id'))
   const plant = plantList.find((plant) => plant.id === plantId)
   console.log(plantId)
   return (
     <>
-      <Header>{plant?.plantname}</Header>
+      <Header hasArrow onClick={() => history.push('/')}>
+        {plant?.plantname}
+      </Header>
       <p>{plant?.plantspecies}</p>
     </>
   )

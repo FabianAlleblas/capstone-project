9e5ebfcaadb00cbaf506564c8ed7ab8381114092
Fabian Alleblas <fabian@alleblas.de>
2020-11-29 17:15:05 +0100

    Add update function to hook

--- frontend/src/hooks/usePlantList.js
+++ frontend/src/hooks/usePlantList.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import getData from '../services/getData'
 import postData from '../services/postData'
+import updateData from '../services/updateData'
 
 export default function usePlantList() {
   const [plantList, setPlantList] = useState(false)
@@ -11,10 +12,18 @@ export default function usePlantList() {
     )
   }, [])
 
-  return { plantList, savePlantData }
+  return { plantList, savePlantData, updatePlantData }
 
   function savePlantData(formData) {
     postData(formData).then((responseData) =>
+      responseData.error
+        ? alert(responseData.error)
+        : setPlantList([...plantList, responseData])
+    )
+  }
+
+  function updatePlantData({ formData, plantId }) {
+    updateData({ formData, plantId }).then((responseData) =>
       responseData.error
         ? alert(responseData.error)
         : setPlantList([...plantList, responseData])
---
+++ frontend/src/services/updateData.js
@@ -0,0 +1,22 @@
+const baseUrl = 'http://urbanplants.local/plant'
+
+export default async function postData({ data, id }) {
+  const myHeaders = new Headers()
+  myHeaders.append('Content-Type', 'application/json')
+  const copy = Object.assign({}, data)
+
+  const raw = JSON.stringify(copy)
+  const requestOptions = {
+    method: 'PUT',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow',
+  }
+  try {
+    const response = await fetch(`${baseUrl}/${id}`, requestOptions)
+    const responseData = response.json()
+    return responseData
+  } catch (error) {
+    return { error: 'The server is down! :(' }
+  }
+}

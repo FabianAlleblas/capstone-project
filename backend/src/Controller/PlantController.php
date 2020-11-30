d8502a770b0a8fef44d9c6e3d49201c3443f529a
Fabian Alleblas <fabian@alleblas.de>
2020-11-30 21:37:06 +0100

    Add remoce functions to repository and controller

--- backend/src/Controller/PlantController.php
+++ backend/src/Controller/PlantController.php
@@ -29,12 +29,14 @@ class PlantController extends AbstractController {
     public function createPlant(Request $request, PlantRepository $plantRepository, SerializerInterface $serializer, ValidatorInterface $validator): JsonResponse {
         $plant = $serializer->deserialize($request->getContent(), Plant::class, 'json');
         $validationResult = $validator->validate($plant);
+        
         if ($validationResult->count() !== 0) {
             return new JsonResponse(
                 ["error" => "Plant data invalid."],
                 JsonResponse::HTTP_BAD_REQUEST
             );
         }
+
         $plantRepository->savePlant($plant);
         return $this->jsonResponse($plant, $serializer);
     }
@@ -43,12 +45,17 @@ class PlantController extends AbstractController {
      * @Route("/plant/{id}", methods={"PUT"})
      */
     public function updatePlant($id, SerializerInterface $serializer, PlantRepository $plantRepository, Request $request, ValidatorInterface $validator): JsonResponse {
-        
-        $plant = $plantRepository->findOneBy(array('id' => $id));
+        $plant = $plantRepository->findOneBy(['id' => $id]);
         $newPlantData = $request->getContent();
-        
         $serializer->deserialize($newPlantData, Plant::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $plant]);
         
+        if ($plant === null) {
+            return new JsonResponse(
+                ["error" => "Plant Id not found."],
+                JsonResponse::HTTP_BAD_REQUEST
+            );
+        }
+
         $validationResult = $validator->validate($plant);
         if ($validationResult->count() !== 0) {
             return new JsonResponse(
@@ -59,6 +66,23 @@ class PlantController extends AbstractController {
 
         $plantRepository->savePlant($plant);
         return $this->jsonResponse($plant, $serializer);
+    }
+
+    /**
+     * @Route("/plant/{id}", methods={"DELETE"})
+     */
+    public function removePlant($id, PlantRepository $plantRepository){
+        $plant = $plantRepository->findOneBy(['id' => $id]);
+
+        if ($plant === null) {
+            return new JsonResponse(
+                ["error" => "Plant Id not found."],
+                JsonResponse::HTTP_BAD_REQUEST
+            );
+        }
+
+        $plantRepository->deletePlant($plant);
+        return new JsonResponse(["Status" => "Item deleted"]);
     }
 
     private function jsonResponse($data, $serializer): JsonResponse {

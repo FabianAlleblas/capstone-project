e495c55057f57c8520bc8f29e68db0249ecd35f7
Fabian Alleblas <fabian@alleblas.de>
2020-12-06 22:31:25 +0100

    Remove typo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import GlobalStyle from './Styles/GlobalStyle'
+import GlobalStyle from './styles/GlobalStyle'
 
 ReactDOM.render(
   <React.StrictMode>

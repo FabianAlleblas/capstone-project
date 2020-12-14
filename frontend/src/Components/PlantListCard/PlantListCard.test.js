ed307f07ab89b6ad5c5c16368def9d79041629e2
Fabian Alleblas <fabian@alleblas.de>
2020-12-14 20:46:54 +0100

    Update PlantListCard tests

--- frontend/src/Components/PlantListCard/PlantListCard.test.js
+++ frontend/src/Components/PlantListCard/PlantListCard.test.js
@@ -2,13 +2,21 @@ import { render } from '@testing-library/react'
 import 'jest-styled-components'
 import PlantListCard from './PlantListCard'
 
+const plant = {
+  name: 'Jack',
+  daysLeft: 4,
+  weeksLeft: 2,
+  waterInterval: 10,
+  fertilizerInterval: 4,
+}
+const onClickMock = jest.fn()
+
 describe('PlantCard', () => {
   it('renders correctly', () => {
-    const { container, getByText } = render(
-      <PlantListCard plant={{ name: 'Jack', daysLeft: 4, weeksLeft: 2 }} />
+    const { getByText } = render(
+      <PlantListCard plant={plant} onClick={onClickMock} />
     )
 
-    expect(container.firstChild).toMatchSnapshot()
     expect(getByText(/Jack/i)).toBeInTheDocument()
   })
 })

8de8ccb2ce490198af281934e427aaeee271b797
Fabian Alleblas <fabian@alleblas.de>
2020-12-16 14:14:21 +0100

    Add password validation

--- frontend/src/hooks/useForm.js
+++ frontend/src/hooks/useForm.js
@@ -2,12 +2,26 @@ import { useState } from 'react'
 
 export default function useForm(initialData) {
   const [formData, setFormData] = useState(initialData ?? {})
+  const [isPasswordValid, setIsPasswordValid] = useState(true)
 
-  return { handleInputChange, formData }
+  return { handleInputChange, validatePassword, formData, isPasswordValid }
 
   function handleInputChange(event) {
     const fieldName = event.target.name
     const fieldValue = event.target.value
     setFormData({ ...formData, [fieldName]: fieldValue })
+  }
+
+  function validatePassword() {
+    if (
+      !formData.password.match(
+        '^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$'
+      )
+    ) {
+      setIsPasswordValid(false)
+      return false
+    }
+    setIsPasswordValid(true)
+    return true
   }
 }

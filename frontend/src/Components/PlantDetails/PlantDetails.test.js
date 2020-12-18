780b5faaa7da880c3d690c5cc2bde48a6acd6d45
Fabian Alleblas <fabian@alleblas.de>
2020-12-18 17:19:42 +0100

    Update tests for PlantDetails

--- frontend/src/Components/PlantDetails/PlantDetails.test.js
+++ frontend/src/Components/PlantDetails/PlantDetails.test.js
@@ -2,6 +2,19 @@ import PlantDetails from './PlantDetails'
 import { render } from '@testing-library/react'
 
 describe('PlantDetailCard', () => {
+  it('renders correctly', () => {
+    const { container } = render(
+      <PlantDetails
+        plant={{
+          species: 'Monstera',
+          specialInfo: 'variegated',
+        }}
+      />
+    )
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
   it('shows the correct name and info', () => {
     const { getByText } = render(
       <PlantDetails

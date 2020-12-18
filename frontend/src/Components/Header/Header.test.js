6aa779f56e230df88fc8bc8de54c426a933bf2d4
Fabian Alleblas <fabian@alleblas.de>
2020-12-18 15:03:04 +0100

    Update Header tests

--- frontend/src/Components/Header/Header.test.js
+++ frontend/src/Components/Header/Header.test.js
@@ -3,28 +3,37 @@ import 'jest-styled-components'
 import Header from './Header'
 
 describe('Header', () => {
-  it('renders correctly', () => {
-    const { getByText } = render(<Header>My Plants</Header>)
+  it('reders with the correct text and logout button', () => {
+    const { getByText, getByRole } = render(
+      <Header showLogoutButton>My Plants</Header>
+    )
 
-    expect(getByText(/My Plants/i)).toBeInTheDocument()
+    expect(getByText('My Plants')).toBeInTheDocument()
+    expect(getByRole('button', { name: /LogoutIcon/i })).toBeInTheDocument()
   })
-  it('renders the correct SVGs', () => {
-    const { getByText, getByTestId } = render(
+  it('reders with the back and edit button', () => {
+    const { getByRole } = render(
       <Header showBackButton showEditButton>
         Helga
       </Header>
     )
 
-    expect(getByText(/Helga/i)).toBeInTheDocument()
-    expect(getByTestId(/arrow-icon/i)).toBeInTheDocument()
-    expect(getByTestId(/edit-icon/i)).toBeInTheDocument()
+    expect(getByRole('button', { name: /ArrowIcon/i })).toBeInTheDocument()
+    expect(getByRole('button', { name: /EditIcon/i })).toBeInTheDocument()
   })
-  it('renders the correct SVG', () => {
-    const { getByText, getByTestId } = render(
-      <Header showLogoutButton>My Plants</Header>
+
+  it('reders with the delete button', () => {
+    const { getByRole } = render(
+      <Header showDeleteButton>Edit your Plant</Header>
     )
 
-    expect(getByText(/My Plants/i)).toBeInTheDocument()
-    expect(getByTestId(/logout-icon/i)).toBeInTheDocument()
+    expect(getByRole('button', { name: /DeleteIcon/i })).toBeInTheDocument()
+  })
+  it('renders the header without a box-shadow', () => {
+    const { getByTestId } = render(<Header secondaryStyle>Add plant</Header>)
+
+    expect(getByTestId('heading-container')).not.toHaveStyle(
+      'box-shadow: 0 0 4px var(--shadow)'
+    )
   })
 })

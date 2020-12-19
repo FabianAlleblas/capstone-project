a59eed7b1cca847c01350078949794a4ef972542
Fabian Alleblas <fabian@alleblas.de>
2020-12-19 13:23:46 +0100

    Add tests to LoginPage

---
+++ frontend/src/Pages/LoginPage/LoginPage.test.js
@@ -0,0 +1,40 @@
+import { render } from '@testing-library/react'
+import user from '@testing-library/user-event'
+import 'jest-styled-components'
+import LoginPage from './LoginPage'
+
+// LoginPage.propTypes = {
+//     userRegistration: PropTypes.func.isRequired,
+//     userLogin: PropTypes.func.isRequired,
+//     userData: PropTypes.object.isRequired,
+//     setUserData: PropTypes.func.isRequired,
+//   }
+
+const userRegistrationMock = jest.fn()
+const userLoginMock = jest.fn()
+const setUserDataMock = jest.fn()
+
+describe('LoginPage', () => {
+  it('renders correctly', () => {
+    const { container } = render(
+      <LoginPage
+        userRegistration={userRegistrationMock}
+        userLogin={userLoginMock}
+        setUserData={setUserDataMock}
+        userData={{}}
+      />
+    )
+    expect(container).toMatchSnapshot()
+  })
+  it('renders the error modal when userData received error response', () => {
+    const { getByText } = render(
+      <LoginPage
+        userRegistration={userRegistrationMock}
+        userLogin={userLoginMock}
+        setUserData={setUserDataMock}
+        userData={{ error: 'lol error' }}
+      />
+    )
+    expect(getByText('lol error')).toBeInTheDocument()
+  })
+})

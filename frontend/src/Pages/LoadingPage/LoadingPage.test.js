3a1e88111ba2ee7f1795f203aa2cb1a4370aa6a3
Fabian Alleblas <fabian@alleblas.de>
2020-12-19 13:16:13 +0100

    Add snapshot to loading page

--- frontend/src/Pages/LoadingPage/LoadingPage.test.js
+++ frontend/src/Pages/LoadingPage/LoadingPage.test.js
@@ -1,9 +1,6 @@
 import { render } from '@testing-library/react'
-import user from '@testing-library/user-event'
 import 'jest-styled-components'
-import { MemoryRouter } from 'react-router-dom'
 import LoginPage from './LoadingPage'
-import LoadingPage from './LoadingPage'
 
 describe('LoadingPage', () => {
   it('renders correctly', () => {
